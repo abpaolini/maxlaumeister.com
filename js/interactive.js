3b9dd00aec094ff90c7e33d9cbbfde3928eccda0
Maximillian Laumeister <max.laumeister@gmail.com>
2014-10-13 22:05:33 -0700

    Add circle effect on click

---
+++ js/interactive.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$("#interactive-go").one("click", function(e) {
+		var inter = $("#interactive-container");
+		var offset = inter.offset();
+		var relX = e.pageX - offset.left;
+		var relY = e.pageY - offset.top;
+		var iw = inter.width();
+		var ih = inter.height();
+		
+		// Calculate optimal circle size
+		var cornerDists = [];
+		cornerDists.push(Math.sqrt(Math.pow(relX, 2) + Math.pow(relY, 2))); // Top left
+		cornerDists.push(Math.sqrt(Math.pow(iw - relX, 2) + Math.pow(relY, 2))); // Top right
+		cornerDists.push(Math.sqrt(Math.pow(iw - relX, 2) + Math.pow(ih - relY, 2))); // Bottom right
+		cornerDists.push(Math.sqrt(Math.pow(relX, 2) + Math.pow(ih - relY, 2))); // Bottom left
+		var circWidth = Math.max.apply(Math, cornerDists) * 2;
+		
+		$("#opening-circle").css({
+			width: circWidth,
+			height: circWidth,
+			left: (relX - circWidth / 2) + "px",
+			top: (relY - circWidth / 2) + "px",
+			"transform": "scale(1, 1)"
+		});
+	});
+});
