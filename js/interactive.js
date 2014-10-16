84713f7dcd79fc2d3c8f1f3a31b94bd5272742d8
Maximillian Laumeister <max.laumeister@gmail.com>
2014-10-15 21:54:47 -0700

    Disable click events for a short while after text changes

--- js/interactive.js
+++ js/interactive.js
@@ -61,9 +61,13 @@ $(document).ready(function() {
 		e.preventDefault();
 		function tryshow2rearm() {
 			clearTimeout(tryshow2timeout);
-			tryshow2timeout = setTimeout(function(){
+			tryshow2timeout = setTimeout(function() {
+				$(".intro-text").css("pointer-events", "none");
 				$(".intro-title-1").fadeOut(500);
 				$(".intro-message-1").fadeOut({duration: 500, complete: function(){
+					setTimeout(function() {
+						$(".intro-text").css("pointer-events", "initial");
+					}, 1000);
 					$(".intro-title-2").fadeIn(500);
 					$(".intro-message-2").fadeIn(500);
 					circlecount2 = 0;
