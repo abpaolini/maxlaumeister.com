eab1ad2a2629e41c562d006b53a1e74610c757be
Maximillian Laumeister <max.laumeister@gmail.com>
2015-08-20 04:22:41 +0000

    Resume now opens in pdf.js

--- _assets/js/resume-modal.js
+++ _assets/js/resume-modal.js
@@ -1,3 +1,28 @@
+var resumeModal = document.getElementById("resume-modal");
+var closeButton = document.getElementById("resume-close");
+
+var iframeEl = document.getElementById("resume-frame");
+
+function closeModal() {
+    resumeModal.style.display = "none";
+    $("body").removeClass("noscroll");
+}
+
+closeButton.onclick = closeModal;
+
+$(resumeModal).click(function() {
+    if(event.target==this) closeModal();
+});
+
+iframeEl.onload = function() {
+    var innerDoc = iframeEl.contentDocument || iframeEl.contentWindow.document;
+    var leftp = innerDoc.getElementById("toolbarViewerLeft");
+    leftp.style.left = "35px";
+    $(leftp).children(".toolbarButtonSpacer").get(0).remove();
+    // Convert links to open in top
+    $('head', innerDoc).append('<base target="_top">');
+};
+
 $(".resume-button").click(function(){
     $("#resume-modal").show();
     $("body").addClass("noscroll");
