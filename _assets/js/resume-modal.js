d191abce68f040719672a3a7b249befebc502168
Maximillian Laumeister <max.laumeister@gmail.com>
2015-09-06 17:56:43 -0700

    Fix resume modal close button on Firefox

--- _assets/js/resume-modal.js
+++ _assets/js/resume-modal.js
@@ -10,15 +10,11 @@ function closeModal() {
 
 closeButton.onclick = closeModal;
 
-$(resumeModal).click(function() {
+$(resumeModal).click(function(event) {
     if(event.target==this) closeModal();
 });
 
 iframeEl.onload = function() {
-    var innerDoc = iframeEl.contentDocument || iframeEl.contentWindow.document;
-    var leftp = innerDoc.getElementById("toolbarViewerLeft");
-    leftp.style.left = "35px";
-    $(leftp).children(".toolbarButtonSpacer").get(0).remove();
     // Convert links to open in top
     $('head', innerDoc).append('<base target="_top">');
 };
