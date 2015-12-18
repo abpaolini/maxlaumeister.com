85fa982c65af9381330cf584b9fd2e52803908fd
Maximillian Laumeister <max.laumeister@gmail.com>
2015-12-18 11:53:41 -0800

    Update Pusher and jQuery, add jQuery fallback, add subresource integrity

--- _assets/js/serviceworker-init.js
+++ _assets/js/serviceworker-init.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
     // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+    //console.log('ServiceWorker registration successful with scope: ',    registration.scope);
   }).catch(function(err) {
     // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
+    //console.log('ServiceWorker registration failed: ', err);
   });
 }
--- serviceworker.js
+++ serviceworker.js
@@ -42,7 +42,7 @@ self.addEventListener('fetch', function(event) {
   // We only want to call event.respondWith() if this is a GET request for the main page.
   if (event.request.method === 'GET' &&
       isRootRequest(event.request.url)) {
-    console.log('Handling fetch event for', event.request.url);
+    //console.log('Handling fetch event for', event.request.url);
     event.respondWith(
       fetch(event.request).catch(function(e) {
         // The catch is only triggered if fetch() throws an exception, which will most likely
@@ -54,7 +54,7 @@ self.addEventListener('fetch', function(event) {
         // Normally, fetch() will consult the browser's HTTP caches before attempting a
         // network request, so in order to trigger offline failure for this sample, we had to
         // use a cache-busting URL parameter to avoid the cache.
-        console.error('Fetch failed; returning offline page instead.', e);
+        //console.error('Fetch failed; returning offline page instead.', e);
         return caches.open(OFFLINE_CACHE).then(function(cache) {
           return cache.match(OFFLINE_URL);
         });
